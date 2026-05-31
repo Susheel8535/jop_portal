1e4f5791e493f4ca51af727a95241575fe114b21
Susheel kumar <susheelkumar08119@gmail.com>
2026-05-31 15:17:52 +0530

    Update API endpoints to production URLs

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+export const USER_API_END_POINT="https://jop-portal-1-iwzc.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://jop-portal-1-iwzc.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://jop-portal-1-iwzc.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT="https://jop-portal-1-iwzc.onrender.com/api/v1/company";
